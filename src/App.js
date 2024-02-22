4c2c4bc30e237aabc7df04ab4367dde25d40cfd8
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-21 23:20:37 -0300

    at3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import { motion } from 'framer-motion'
 
+import image1 from './img/1.jpg'
+import image2 from './img/2.jpg'
+import image3 from './img/3.png'
+import image4 from './img/4.png'
+
+const images = [image1, image2, image3, image4]
+
 function App() {
   return (
     <div className="App">
-      <motion.h1 animate={{x: 200, y: 100}}>a</motion.h1>
+
+      <motion.div className='carrossel1'>
+        <motion.div className='inner'>
+
+          {images.map(image => (
+            <motion.div className='item' key={image}>
+              <img src={image} alt='Img'/>
+            </motion.div>
+          ))}
+
+        </motion.div>
+      </motion.div>
+
     </div>
   );
 }
